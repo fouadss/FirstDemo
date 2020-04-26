e6580cdf5bc0b067fabdf2717ba412674bfbca67
fouad <fsidisaid@gmail.com>
2020-04-25 20:27:42 -0400

    inetlij training

---
+++ src/basic/FirstClass.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class FirstClass {
+
+
+    public static void main(String[] arge){
+
+        System.out.println(" hello world");
+    }
+}
---
+++ src/basic/Training.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class Training {
+
+
+    public static void main(String[] args) {
+       // Find out Largest Number from below 55,65,75
+       // output: 75
+        int a=55;
+        int b= 65;
+        int c;
+    }
+}
